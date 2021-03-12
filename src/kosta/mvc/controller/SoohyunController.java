6e3f425c72cb02899c3d1f5ac3cffc730724ff0a
wshyu <wshyu@DESKTOP-BEFC6PN>
2021-03-12 11:41:49 +0900

    세번째 수정

---
+++ src/kosta/mvc/controller/SoohyunController.java
@@ -0,0 +1,7 @@
+package kosta.mvc.controller;
+
+public class SoohyunController {
+	public void test() {
+		System.out.println("수현 추가했어요");
+	}
+}
--- src/kosta/mvc/service/TestService.java
+++ src/kosta/mvc/service/TestService.java
@@ -1,5 +1,7 @@
 package kosta.mvc.service;
 
 public class TestService {
-
+	public void service() {
+		System.out.println("서로가 수정하고 추가하는데 같은 파일을 건들지 않는 실습");
+	}
 }
